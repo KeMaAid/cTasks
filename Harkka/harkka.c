fa73535893c237ace0686aa57dba06f3c93fe5cd
KeMaAid <konsta.keski-mattinen@student.lut.fi>
2021-02-24 16:02:26 +0200

    init harkka

---
+++ Harkka/harkka.c
@@ -0,0 +1,9 @@
+/*******************************************************************/
+/* CT60A2500 C-ohjelmoinnin perusteet
+ * Otsikkotiedot:
+ * Tekijä: Konsta Keski-Mattinen
+ * Opiskelijanumero: 0568752
+ * Päivämäärä: 
+ * Yhteistyö ja lähteet, nimi ja yhteistyön muoto:
+ */
+/*******************************************************************/
