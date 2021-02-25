5ee3cf3bf25cdc822001bf8153a4c973bd0846ea
KeMaAid <konsta.keski-mattinen@student.lut.fi>
2021-02-25 15:04:09 +0200

    basic muisti

--- Viikko7/T3/muisti.c
+++ Viikko7/T3/muisti.c
@@ -7,4 +7,17 @@
  * Yhteistyö ja lähteet, nimi ja yhteistyön muoto:
  */
 /*******************************************************************/
+#include <stdlib.h>
+#include "muisti.h"
 
+int* varaaMuistiaNumerolle(int size){
+    int *ptr;
+    if((ptr = (int*)malloc(sizeof(int)*size))== NULL){
+        perror("Muistin varaus epäonnistui!\n");
+        exit(0);
+    }
+    return ptr;
+}
+void vapautaMuisti(int* ptr){
+    free(ptr);
+}
